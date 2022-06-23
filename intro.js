ed38104fa250ab88ffe23848be7d2ac068f75f81
Nwadike Joseph <jcyoung.mjc44@gmail.com>
2022-06-23 16:05:21 +0100

    first commit

---
+++ intro.js
@@ -0,0 +1,8 @@
+let fname = "Joseph";
+      let lname = "Nwadike";
+      let full_name = "Nwadike Odira Joseph";
+      let height = "5.6ft";
+      let country = "Nigeria";
+      
+      window.alert(`First Name: ${fname}\n \nLast Name: ${lname}\n \nFull Name: ${full_name}\n \nHeight: ${height}\n \nCountry: ${country}`)
+
